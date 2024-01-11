48e7641f5008d11963f4ac51c10baf88fde41a01
Marisela <mariselaalejandragomez@gmail.com>
2024-01-11 14:43:35 -0800

    finished phase 2

--- src/components/SelectedCoffeeBean.jsx
+++ src/components/SelectedCoffeeBean.jsx
@@ -1,14 +1,13 @@
-import {useContext, useState, useEffect} from "react";
+import { useContext, useState, useEffect } from "react";
 import { CoffeeContext } from "../context/CoffeeContext";
 const SelectedCoffeeBean = () => {
-  const {coffeeBean, setCoffeeBean} = useContext(CoffeeContext)
-  console.log("coffee bean is: ", coffeeBean)
-  return (
-    <div className="selected-coffee">
-      <h2>Current Selection: {coffeeBean.name} </h2>
-
-    </div>
-  );
-}
+	const { coffeeBean, setCoffeeBean } = useContext(CoffeeContext);
+	// console.log("coffee bean is: ", coffeeBean)
+	return (
+		<div className='selected-coffee'>
+			<h2>Current Selection: {coffeeBean.name} </h2>
+		</div>
+	);
+};
 
 export default SelectedCoffeeBean;
--- src/components/SetCoffeeBean.jsx
+++ src/components/SetCoffeeBean.jsx
@@ -1,21 +1,25 @@
+import { useContext } from "react";
+import { CoffeeContext } from "../context/CoffeeContext";
+
 const SetCoffeeBean = ({ coffeeBeans }) => {
-  return (
-    <div className="set-coffee-bean">
-      <h2>Select a Coffee Bean</h2>
-      <select
-        name="coffee-bean"
-      >
-        {coffeeBeans.map(bean => (
-          <option
-            key={bean.id}
-            value={bean.id}
-          >
-            {bean.name}
-          </option>
-        ))}
-      </select>
-    </div>
-  );
-}
+	// console.log("rendering setcoffee");
+	const { coffeeBean, setCoffeeBeanId } = useContext(CoffeeContext);
+	console.log(coffeeBean);
+	return (
+		<div className='set-coffee-bean'>
+			<h2>Select a Coffee Bean</h2>
+			<select
+				name='coffee-bean'
+				onChange={(e) => setCoffeeBeanId(e.target.value)}
+			>
+				{coffeeBeans.map((bean) => (
+					<option key={bean.id} value={bean.id}>
+						{bean.name}
+					</option>
+				))}
+			</select>
+		</div>
+	);
+};
 
 export default SetCoffeeBean;
